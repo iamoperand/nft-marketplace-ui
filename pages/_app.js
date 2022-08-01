bcb5e9473a0bc356e6ceb0cca226e19436c92869
nikhil arora <narora200@gmail.com>
2022-08-02 00:45:29 +0530

    feat: setup tailwind and initial layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "styles/globals.css"
+import Head from "next/head"
+import { MoralisProvider } from "react-moralis"
+
+import Header from "components/header"
+import env from "constants/env"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={env.moralis.appId} serverUrl={env.moralis.serverUrl}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
